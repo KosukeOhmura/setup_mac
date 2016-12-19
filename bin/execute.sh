6189730acb8d02aa3c021c1137d3e5a518506858
Kosuke Ohmura <bnpb81@gmail.com>
2016-12-19 19:25:52 +0900

    fix wrong filename

--- bin/execute.sh
+++ bin/execute.sh
@@ -4,7 +4,7 @@ echo "$0 Started..."
 chmod 755 $(dirname $0)/*.sh
 $(dirname $0)/install_xcode.sh
 $(dirname $0)/install_brew.sh
-$(dirname $0)/run_gem.sh
+$(dirname $0)/apply_rbenv.sh
 $(dirname $0)/run_brew.sh
 $(dirname $0)/run_mas.sh
 $(dirname $0)/apply_defaults.sh
