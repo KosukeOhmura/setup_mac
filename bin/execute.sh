1c61101717a8021a24ed9c415fc471926a833caa
Kosuke Ohmura <bnpb81@gmail.com>
2017-04-03 10:58:37 +0900

    fix failing shell script order

--- bin/execute.sh
+++ bin/execute.sh
@@ -5,9 +5,8 @@ chmod 755 $(dirname $0)/*.sh
 $(dirname $0)/apply_symbolic_links.sh
 $(dirname $0)/install_xcode.sh
 $(dirname $0)/install_brew.sh
-$(dirname $0)/apply_rbenv.sh
-$(dirname $0)/apply_pyenv.sh
 $(dirname $0)/run_brew.sh
 $(dirname $0)/run_mas.sh
+$(dirname $0)/apply_rbenv.sh
+$(dirname $0)/apply_pyenv.sh
 $(dirname $0)/apply_defaults.sh
-
